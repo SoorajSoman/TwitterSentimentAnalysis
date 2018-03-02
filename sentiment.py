e6fb7ce1928acf915de423bc2ddda371fa4d27fd
Cyber <soorajsoman22@gmail.com>
2018-03-02 11:13:14 +0530

    Initial commit

---
+++ sentiment.py
@@ -0,0 +1,25 @@
+import tweepy
+from textblob import TextBlob
+import csv
+import pandas as pd  
+import numpy as np
+import matplotlib.pyplot as plt
+
+consumer_key="YOUR-CONSUMER-KEY"
+consumer_secret="YOUR-CONSUMER-SECRET-KEY"
+
+access_token="YOUR-ACCESS-TOKEN"
+access_token_secret="YOUR-ACCESS-TOKEN-SECRET"
+
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api=tweepy.API(auth)
+
+public_tweets=api.search("Trump")
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis=TextBlob(tweet.text)
+    print(analysis.sentiment)
+   
